cdbe0e716ebf4932c2b94a03ad0580f670033893
Ashley Williams <ashley666ashley@gmail.com>
2016-06-23 18:55:13 -0400

    add streams bug bandaid, benchmarking - change to on 'data' not 'readable' because of likely streams bug - add benchmarking

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 const ChangesStream = require('changes-stream');
 const Marky = require('marky-markdown');
 const Request = require('request');
+const Summarize = require('summary-statistics');
 
-const db = 'https://skimdb.npmjs.com/registry/';
+const db = 'https://replicate.npmjs.com';
+
+var parseTimes = [];
 
 var changes = new ChangesStream({
   db: db,
   include_docs: true
 });
 
+var printHRTime = function(response) {
+  return response/1000000 + "ms";
+};
+
 var parseReadme = function (change) {
-  Marky(change.doc.readme || "", { pkg: change.doc }).html();
-  console.log("parsed: " + change.doc.name + ", " + change.seq);
+  const start = process.hrtime();
+  var readme = (change.doc.readme || "").toString();
+  Marky(readme, { pkg: change.doc }).html();
+  const elapsed = process.hrtime(start)[1];
+  console.log("parsed: " + change.doc.name + ", " + change.seq +
+              " [" + printHRTime(elapsed) + "]");
+  return elapsed/1000000;
 };
 
 Request.get(db, function(err, req, body) {
   var end_sequence = JSON.parse(body).update_seq;
-  changes.on('readable', function () {
-    var change = this.read();
+  changes.on('data', function (change) {
+    // stop after we've parsed all the things
     if (change.seq >= end_sequence) {
+      console.log(Summarize(parseTimes));
       process.exit(0);
     }
-    parseReadme(change);
+    // do not parse CouchDB design docs
+    if (change.doc.name) {
+      parseTimes.push(parseReadme(change));
+    }
   });
 });
